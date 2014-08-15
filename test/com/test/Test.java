af73cd3e57e769eb0e750c2bce55aa7626ee639f
weige <weige@126.com>
2014-08-15 23:39:21 +0800

    Test for git in eclipse.

--- test/com/test/Test.java
+++ test/com/test/Test.java
@@ -1,13 +1,12 @@
 package com.test;
 
-import com.dec.model.Customer;
 
 public class Test {
 	
 	@org.junit.Test
 	public void test()
 	{
-		Customer c = new Customer();
 		System.out.println("This is the test SourceFolder!");
+		System.out.println("Test for git in eclipse.");
 	}
 }
